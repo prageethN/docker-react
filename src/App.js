7b734c94982d1507ae2146702da89b5c72de9da6
Prageeth Gangabada <prageeth.gangabada@ifsworld.com>
2020-03-16 13:35:22 +0530

    changed the full name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Prageeth Gangabada
+          Hello, Prageeth Nuwan Gangabada
         </p>
         
       </header>
